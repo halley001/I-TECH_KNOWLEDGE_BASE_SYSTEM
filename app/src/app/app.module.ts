4a540fea0a85587ce9f17b75cbdbd2bc4a3a8422
halley001 <halleymail100@gmail.com>
2023-08-31 17:47:43 +0100

    Navbar updated

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,37 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FrontpageComponent } from './components/frontpage/frontpage.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { HeaderComponent } from './components/header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
-
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FrontpageComponent,
-    LoginComponent,
-    RegisterComponent,
-    HeaderComponent,
+    NavbarComponent,
     HomeComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatSlideToggleModule,
-    RouterModule.forRoot([
-      {path:'login', component:LoginComponent},
-      {path:'register', component: RegisterComponent},
-      {path:'home', component:HomeComponent},
-      {path: '', redirectTo: 'home', pathMatch: 'full'},
-      {path: '**', redirectTo: 'home', pathMatch: 'full'}
-    ])
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
